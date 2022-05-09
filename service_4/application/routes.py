32dc79b9e972c4dbd50af9f6c6e97ae767fb3b62
Art <art.aliu@googlemail.com>
2022-05-09 14:04:10 +0000

    Art:service 1,2,3 working. Serivice 4 needs work

--- service_4/application/routes.py
+++ service_4/application/routes.py
@@ -1,19 +1,48 @@
 from application import app
-from flask import Flask, request, jsonify, Response
-import requests
-
-notes = {0: "Has great potential", 1: "Will never make it"
-special = "Shooting, Passing, Speed"
+from flask import Flask, request, jsonify
+import requests, random
 
 
-@app.route('/extra', method=['POST'])
-def extra():
-    get_attribute= request.get['attribute']
+scout_ratings = {
+    'attribute': {
+        'Speed': 5,
+        'Strength': 3,
+        'Endurance' : 2
+    },
+    'skill': {
+        'Shooting': 5,
+        'Passing': 3,
+        'Tackling': 3
+    }
+}
+@app.route('/post/rating', methods=['POST'])
+def post_rating():
+    attribute = request.json['attribute']
+    skill = request.json['skill']
+    
+    rating = scout_ratings['attribute'][attribute] + scout_ratings['skill'][skill]
 
-    for special in get_attribute:
-        if special in get_attribute:
-            return Response(f"{notes[0)}", mimetype = 'text/plain')
-        else:
-            return Response(f"{notes[1)}", mimetype = 'text/plain')
+    return jsonify(rating)
+    
+
+    
 
 
+
+
+# notes = {0: "Has great potential", 1: "Will never make it"
+# special = "Shooting, Passing, Speed"
+
+
+# @app.route('/extra', method=['POST'])
+# def extra():
+#     attributes = request.get_json["Attribute"]
+#     ratings = request.get_json["Rating"]
+
+
+
+#     for special in attributes:
+#         if special in get_attribute:
+#             return Response(f"{notes[0)}", mimetype = 'text/plain')
+#         else:
+#             return Response(f"{notes[1)}", mimetype = 'text/plain')
