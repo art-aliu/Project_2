7aa5d88ce32179b535761f1c13af9f9157cf56a2
Art <art.aliu@googlemail.com>
2022-05-11 11:00:08 +0000

    Art:changes to test.sh

--- test.sh
+++ test.sh
@@ -1,11 +1,4 @@
-# sudo apt-get install python3-venv -y
-# python3 -m venv venv
-
-# source venv/bin/activate
-
-# pip3 install -r requirements.txt
-# python3 -m pytest --cov --cov-config=.coveragerc --cov-report term-missing --cov-report xml:coverage.xml --junitxml junit.xml
-
+#!/bin/bash
 python3 -m venv venv
 
 ls -l
