fb8f42924b1d3fc8be681940dbaf1c739b9ec1ea
Art <art.aliu@googlemail.com>
2022-05-11 11:05:11 +0000

    Art:changes to test.sh again

--- test.sh
+++ test.sh
@@ -1,22 +1,37 @@
 #!/bin/bash
-python3 -m venv venv
+# python3 -m venv venv
 
-ls -l
+# ls -l
 
-source venv/bin/activate
+# source venv/bin/activate
 
+# declare -a directories=("service_1" "service_2" "service_3" "service_4")
+
+# for dir in "${directories[@]}"
+
+# do
+
+#     cd ${dir}
+
+#     pip3 install -r requirements.txt
+
+#     python3 -m pytest --cov=application
+
+#     cd ..
+
+# done
+
+#!/bin/bash
 declare -a directories=("service_1" "service_2" "service_3" "service_4")
-
 for dir in "${directories[@]}"
-
 do
-
-    cd ${dir}
-
-    pip3 install -r requirements.txt
-
-    python3 -m pytest --cov=application
-
-    cd ..
-
+  cd ${dir}
+  sudo apt-get update
+  sudo apt-get install python3 python3-pip python3-venv
+  python3 -m venv venv
+  source venv/bin/activate
+  pip3 install -r test_requirements.txt
+  python3 -m pytest --cov=application --cov-report=xml --junitxml=junit/test-results.xml
+  deactivate
+  cd ..
 done
